17827f7cc483919e7902190791f816d20d4df534
BlackLusterSoldier7 <ngbc2@outlook.com>
2023-10-11 12:49:37 +0200

    3 lagen archit

--- C#/WebshopC#Console/WebshopC#Console/ConsoleUserInterface.cs
+++ C#/WebshopC#Console/WebshopC#Console/ConsoleUserInterface.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebshopC_Console
+﻿namespace WebshopC_Console
 {
     public class ConsoleUserInterface
     {
