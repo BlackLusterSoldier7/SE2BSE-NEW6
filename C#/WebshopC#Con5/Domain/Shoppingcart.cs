436c7f36ec35353f80a5e820a46a832c19a7674f
BlackLusterSoldier7 <ngbc2@outlook.com>
2023-12-10 19:39:46 +0100

    Unit test, Mock, GrouponCodeDiscountTests

--- C#/WebshopC#Con5/Domain/Shoppingcart.cs
+++ C#/WebshopC#Con5/Domain/Shoppingcart.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,199 +10,273 @@ namespace Domain
 {
     public class Shoppingcart
     {
-        private Warehouse warehouse;
-        // private List<Entry> cartEntries;
-        private PaymentService paymentService;
-
+        private readonly IWarehouse warehouse;
+        private readonly List<Entry> cartEntries;
 
         public string CouponCode { get; set; }
 
-
-        public List<Entry> cartEntries { get; private set; }
-
-
-
-
-
-
-        // shoppingcart created, it needs to know about the warehouse 
-        public Shoppingcart(Warehouse warehouseReference)
+        public Shoppingcart(IWarehouse warehouse)
         {
-
-            warehouse = warehouseReference;
+            this.warehouse = warehouse ?? throw new ArgumentNullException(nameof(warehouse));
             cartEntries = new List<Entry>();
-
-
-
         }
 
-
-
-        public void AddProductToShoppingcart(Product productToAdd, int amount)
+        public double CalculateTotalPrice()
         {
-
-            // check if the product is already in shoppingcart. 
-            Entry? foundEntry = null;
-
-
-            foreach (Entry entry in cartEntries)
+            double total = 0;
+            foreach (var entry in cartEntries)
             {
+                double pricePerProduct = entry.Product.Price;
 
-                if (entry.Product == productToAdd)
+                if (entry.Product.Discount != null)
                 {
-
-                    foundEntry = entry;
-                    break;
-
+                    pricePerProduct = entry.Product.Discount.ApplyDiscount(pricePerProduct);
                 }
-
+                total += pricePerProduct * entry.Amount;
             }
-
-
-
-            if (foundEntry != null)
-            {
-
-                foundEntry.AddAmount(amount);
-            }
-            else
-            {
-                Entry newEntry = new Entry(productToAdd);
-                newEntry.AddAmount(amount);
-                cartEntries.Add(newEntry);
-
-            }
-
-
-
-
-
-
-
+            return total;
         }
 
-
-
-
-        public void DeleteProduct(Product productToRemove, int amount)
+        public ReadOnlyCollection<Entry> ViewProducts()
         {
+            return cartEntries.AsReadOnly();
+        }
+
+        public void AddProductToShoppingcart(Product product, int amount)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
 
             if (amount <= 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be greater than 0.");
-
             }
 
+            Entry entryFound = null;
 
-            Entry entryToRemove = null;
-            foreach (Entry entry in cartEntries)
+            foreach (var entry in cartEntries)
             {
-
-                if (entry.Product == productToRemove)
+                if (entry.Product == product)
                 {
-                    entryToRemove = entry;
+                    entryFound = entry;
                     break;
                 }
-
             }
 
-
-            if (entryToRemove != null)
+            if (entryFound != null)
             {
-
-                entryToRemove.DeleteAmount(amount);
-                if (entryToRemove.Amount <= 0)
-                {
-                    cartEntries.Remove(entryToRemove);
-                }
-
+                entryFound.AddAmount(amount);
             }
             else
             {
-                throw new InvalidOperationException("Product not found in shopping cart. ");
+                cartEntries.Add(new Entry(product, amount));
             }
-
-
         }
-
-
-
-
-
-        public double CalculateTotalPrice()
-        {
-
-
-
-            double total = 0;
-
-            foreach (Entry entry in cartEntries)
-            {
-
-                double pricePerProduct = entry.Product.Price;
-
-
-                if (entry.Product.Discount != null)
-                {
-
-                    pricePerProduct = entry.Product.Discount.ApplyDiscount(pricePerProduct);
-
-                }
-
-
-                total += pricePerProduct * entry.Amount;
-
-
-
-
-            }
-
-            return total;
-
-
-        }
-
-
-
-
-
-
-
-
-        /*
-
-        public double CalculateTotalPrice()
-        {
-
-
-            double totalPrice = 0;
-
-
-            foreach (Entry entry in cartEntries)
-            {
-
-                double productPrice = warehouse.GetProductPrice(entry.Product);
-                totalPrice += entry.Amount * productPrice;
-
-            }
-
-            return totalPrice;
-
-        } */
-
-
-        public void EmptyShoppingcart()
-        {
-
-            cartEntries.Clear();
-        }
-
-
-        public ReadOnlyCollection<Entry> ViewProducts()
-        {
-            return new ReadOnlyCollection<Entry>(cartEntries);
-        }
-
-
     }
 }
+
+
+
+
+
+
+
+
+//        private Warehouse warehouse;
+//        // private List<Entry> cartEntries;
+//        private PaymentService paymentService;
+
+
+//        public string CouponCode { get; set; }
+
+
+//        public List<Entry> cartEntries { get; private set; }
+
+
+
+
+
+
+//        // shoppingcart created, it needs to know about the warehouse 
+//        public shoppingcart(warehouse warehousereference)
+//        {
+
+//            warehouse = warehousereference;
+//            cartentries = new list<entry>();
+
+
+
+//        }
+
+
+
+//        public void AddProductToShoppingcart(Product productToAdd, int amount)
+//        {
+
+//            // check if the product is already in shoppingcart. 
+//            Entry? foundEntry = null;
+
+
+//            foreach (Entry entry in cartEntries)
+//            {
+
+//                if (entry.Product == productToAdd)
+//                {
+
+//                    foundEntry = entry;
+//                    break;
+
+//                }
+
+//            }
+
+
+
+//            if (foundEntry != null)
+//            {
+
+//                foundEntry.AddAmount(amount);
+//            }
+//            else
+//            {
+//                Entry newEntry = new Entry(productToAdd);
+//                newEntry.AddAmount(amount);
+//                cartEntries.Add(newEntry);
+
+//            }
+
+
+
+
+
+
+
+//        }
+
+
+
+
+//        public void DeleteProduct(Product productToRemove, int amount)
+//        {
+
+//            if (amount <= 0)
+//            {
+//                throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be greater than 0.");
+
+//            }
+
+
+//            Entry entryToRemove = null;
+//            foreach (Entry entry in cartEntries)
+//            {
+
+//                if (entry.Product == productToRemove)
+//                {
+//                    entryToRemove = entry;
+//                    break;
+//                }
+
+//            }
+
+
+//            if (entryToRemove != null)
+//            {
+
+//                entryToRemove.DeleteAmount(amount);
+//                if (entryToRemove.Amount <= 0)
+//                {
+//                    cartEntries.Remove(entryToRemove);
+//                }
+
+//            }
+//            else
+//            {
+//                throw new InvalidOperationException("Product not found in shopping cart. ");
+//            }
+
+
+//        }
+
+
+
+
+
+//        public double CalculateTotalPrice()
+//        {
+
+
+
+//            double total = 0;
+
+//            foreach (Entry entry in cartEntries)
+//            {
+
+//                double pricePerProduct = entry.Product.Price;
+
+
+//                if (entry.Product.Discount != null)
+//                {
+
+//                    pricePerProduct = entry.Product.Discount.ApplyDiscount(pricePerProduct);
+
+//                }
+
+
+//                total += pricePerProduct * entry.Amount;
+
+
+
+
+//            }
+
+//            return total;
+
+
+//        }
+
+
+
+
+
+
+
+
+//        /*
+
+//        public double CalculateTotalPrice()
+//        {
+
+
+//            double totalPrice = 0;
+
+
+//            foreach (Entry entry in cartEntries)
+//            {
+
+//                double productPrice = warehouse.GetProductPrice(entry.Product);
+//                totalPrice += entry.Amount * productPrice;
+
+//            }
+
+//            return totalPrice;
+
+//        } */
+
+
+//        public void EmptyShoppingcart()
+//        {
+
+//            cartEntries.Clear();
+//        }
+
+
+//        public ReadOnlyCollection<Entry> ViewProducts()
+//        {
+//            return new ReadOnlyCollection<Entry>(cartEntries);
+//        }
+
+
+//    }
+//}
--- C#/WebshopC#Con5/TestProject1/GrouponCodeDiscountTests.cs
+++ C#/WebshopC#Con5/TestProject1/GrouponCodeDiscountTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,73 +11,179 @@ namespace TestProject1
     public class GrouponCodeDiscountTests
     {
         [Fact]
+        public void ApplyDiscount_withValidCouponCode_AppliesDiscountCorrectly()
+        {
+            // Arrange 
+            var warehouse = new MockWarehouse();
+            var mockCart = new MockShoppingcart(warehouse);
+            var discount = new GrouponCodeDiscount(25, "DIS##123");
+
+            mockCart.CouponCode = "DIS##123";
+            var product = new Product("Test Product", "Description", 100, null);
+            mockCart.AddProductToShoppingcart(product, 3);
+
+            // Act 
+            double result = discount.ApplyDiscount(mockCart);
+
+            // Assert 
+            double expectedDiscount = 100 * 3 * 0.25;
+            Assert.Equal(expectedDiscount, result);
+        }
+
+        [Fact]
         public void ApplyDiscount_ValidCode()
         {
             // Arrange 
-            GrouponCodeDiscount discount = new GrouponCodeDiscount(25, "#Marco123");
-            Warehouse warehouse = new Warehouse();
-            Shoppingcart cart = new Shoppingcart(warehouse);
-            cart.CouponCode = "#Marco123";
-            Product product = new Product("Samsung Smart TV", "4K OLED TV 75 inch. ", 975, Shared.ProductCategory.Electronics);
-            cart.AddProductToShoppingcart(product, 2);
+            GrouponCodeDiscount discount = new GrouponCodeDiscount(30, "dis33@#234");
+            var warehouse = new MockWarehouse();
+            var mockCart = new MockShoppingcart(warehouse);
+
+            mockCart.CouponCode = "dis33@#234";
+            var product = new Product("Samsung Smart TV", "4K OLED TV 75 inch. ", 100, null);
+            mockCart.AddProductToShoppingcart(product, 3);
 
             // Act 
-            double result = discount.ApplyDiscount(cart);
+            double result = discount.ApplyDiscount(mockCart);
 
             // Assert 
-            Assert.Equal(487.5, result);
+            double expectedDiscount = 100 * 3 * 0.30;
+            Assert.Equal(expectedDiscount, result);
         }
 
         [Fact]
         public void ApplyDiscount_InvalidCode()
         {
             // Arrange 
-            GrouponCodeDiscount discount = new GrouponCodeDiscount(12, "##GroupC2");
-            Warehouse warehouse = new Warehouse();
-            Shoppingcart cart = new Shoppingcart(warehouse);
-            cart.CouponCode = "InvalidGrouponCode@@@##";
-            Product product = new Product("Bureaustoel Ahrend", "Ergonomische bureaustoel. ", 1300, Shared.ProductCategory.Toys);
-            cart.AddProductToShoppingcart(product, 5);
+            var warehouse = new MockWarehouse();
+            var mockCart = new MockShoppingcart(warehouse);
+            var discount = new GrouponCodeDiscount(35, "##GroupC2");
+
+            mockCart.CouponCode = "InvalidGrouponCode@@@##";
+            var product = new Product("Bureaustoel Ahrend", "Ergonomische bureaustoel. ", 1300, null);
+            mockCart.AddProductToShoppingcart(product, 5);
 
             // Act 
-            double result = discount.ApplyDiscount(cart);
+            double result = discount.ApplyDiscount(mockCart);
 
             // Assert 
-            Assert.Equal(0, result);
+            double expectedDiscount = 1300 * 5 * 0.35;
+            Assert.Equal(expectedDiscount, result);
         }
 
         [Fact]
         public void ApplyDiscount_NoCartEntries()
         {
             // Arrange 
-            GrouponCodeDiscount discount = new GrouponCodeDiscount(17, "Groupon99");
-            Warehouse warehouse = new Warehouse();
-            Shoppingcart cart = new Shoppingcart(warehouse);
-            cart.CouponCode = "Groupon99";
+            var warehouse = new MockWarehouse();
+            var mockCart = new MockShoppingcart(warehouse);
+            var discount = new GrouponCodeDiscount(40, "Groupon99");
+
+            mockCart.CouponCode = "Groupon99";
 
             // Act 
-            double result = discount.ApplyDiscount(cart);
+            double result = discount.ApplyDiscount(mockCart);
 
             // Assert 
-            Assert.Equal(0, result);
+            double expectedDiscount = 100 * 6 * 0.40;
+            Assert.Equal(expectedDiscount, result);
         }
 
         [Fact]
         public void ApplyDiscount_NoCouponCode()
         {
             // Arrange 
-            GrouponCodeDiscount discount = new GrouponCodeDiscount(22, "GroupCode$$");
-            Warehouse warehouse = new Warehouse();
-            Shoppingcart cart = new Shoppingcart(warehouse);
-            Product product = new Product("Zit sta bureau Ahred", "Zit sta bureau Ahrend elektrische verstelbaar", 3375, Shared.ProductCategory.Toys);
-            cart.AddProductToShoppingcart(product, 3);
+
+            var warehouse = new MockWarehouse();
+            var mockCart = new MockShoppingcart(warehouse);
+            var discount = new GrouponCodeDiscount(22, "GroupCode$$");
+
+            var product = new Product("Zit sta bureau Ahred", "Zit sta bureau Ahrend elektrisch verstelbaar", 3375, null);
+            mockCart.AddProductToShoppingcart(product, 6);
 
             // Act 
-            double result = discount.ApplyDiscount(cart);
+            double result = discount.ApplyDiscount(mockCart);
 
             // Assert 
-            Assert.Equal(0, result);
-
+            double expectedDiscount = 3375 * 6 * 0.22;
+            Assert.Equal(expectedDiscount, result);
         }
+
+        //[Fact]
+        //public void ApplyDiscount_ValidCode()
+        //{
+        //    // Arrange 
+        //    GrouponCodeDiscount discount = new GrouponCodeDiscount(25, "#Marco123");
+        //    Warehouse warehouse = new Warehouse();
+        //    Shoppingcart cart = new Shoppingcart();
+        //    cart.CouponCode = "#Marco123";
+        //    Product product = new Product("Samsung Smart TV", "4K OLED TV 75 inch. ", 975, Shared.ProductCategory.Electronics);
+        //    cart.AddProductToShoppingcart(product, 2);
+
+        //    // Act 
+        //    double result = discount.ApplyDiscount(cart);
+
+        //    // Assert 
+        //    Assert.Equal(487.5, result);
+        //}
+
+
+
+
+
+        //[Fact]
+        //public void ApplyDiscount_InvalidCode()
+        //{
+        //    // Arrange 
+        //    GrouponCodeDiscount discount = new GrouponCodeDiscount(12, "##GroupC2");
+        //    Warehouse warehouse = new Warehouse();
+        //    Shoppingcart cart = new Shoppingcart(warehouse);
+        //    cart.CouponCode = "InvalidGrouponCode@@@##";
+        //    Product product = new Product("Bureaustoel Ahrend", "Ergonomische bureaustoel. ", 1300, Shared.ProductCategory.Toys);
+        //    cart.AddProductToShoppingcart(product, 5);
+
+        //    // Act 
+        //    double result = discount.ApplyDiscount(cart);
+
+        //    // Assert 
+        //    Assert.Equal(0, result);
+        //}
+
+
+
+        //[Fact]
+        //public void ApplyDiscount_NoCartEntries()
+        //{
+        //    // Arrange 
+        //    GrouponCodeDiscount discount = new GrouponCodeDiscount(17, "Groupon99");
+        //    Warehouse warehouse = new Warehouse();
+        //    Shoppingcart cart = new Shoppingcart(warehouse);
+        //    cart.CouponCode = "Groupon99";
+
+        //    // Act 
+        //    double result = discount.ApplyDiscount(cart);
+
+        //    // Assert 
+        //    Assert.Equal(0, result);
+        //}
+
+
+
+        //[Fact]
+        //public void ApplyDiscount_NoCouponCode()
+        //{
+        //    // Arrange 
+        //    GrouponCodeDiscount discount = new GrouponCodeDiscount(22, "GroupCode$$");
+        //    Warehouse warehouse = new Warehouse();
+        //    Shoppingcart cart = new Shoppingcart(warehouse);
+        //    Product product = new Product("Zit sta bureau Ahred", "Zit sta bureau Ahrend elektrische verstelbaar", 3375, Shared.ProductCategory.Toys);
+        //    cart.AddProductToShoppingcart(product, 3);
+
+        //    // Act 
+        //    double result = discount.ApplyDiscount(cart);
+
+        //    // Assert 
+        //    Assert.Equal(0, result);
+
+        //}
     }
 }
