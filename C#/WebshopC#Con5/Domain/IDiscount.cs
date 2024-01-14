7b8adca2b3cfb89ae87d26f4b438e47fe7bcd01a
BlackLusterSoldier7 <ngbc2@outlook.com>
2024-01-14 17:16:27 +0100

    Deleted Discount2, interface IDiscount mogelijke uitbreidingen

--- C#/WebshopC#Con5/Domain/IDiscount.cs
+++ C#/WebshopC#Con5/Domain/IDiscount.cs
@@ -9,5 +9,16 @@ namespace Domain
     public interface IDiscount
     {
         double ApplyDiscount(Shoppingcart cart);
+
+        // Eventuele uitbreidingen. Door tijdsgebrek niet geimplementeerd. 
+        // check conditions, minimum cart value, specific items present in the cart etc. 
+        // bool IsValidForCart(Shoppingcart cart);
+
+        // current discount can be combined with another discount?
+        // bool CanCombineWith(IDiscount otherDiscount); 
+
+        // ApplyDiscountToItem can discounts apply to spefic items insteand of the whole cart
+        // bedoeld voor een individu product. 
+        //double ApplyDiscountToItem(Product item); 
     }
 }
